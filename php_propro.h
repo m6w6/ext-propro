0375bec5a1b40e64a052689440e7e1303230bf93
Michael Wallner <mike@php.net>
2013-02-16 23:11:08 +0100

    start with a low version

--- php_propro.h
+++ php_propro.h
@@ -16,7 +16,7 @@
 extern zend_module_entry propro_module_entry;
 #define phpext_propro_ptr &propro_module_entry
 
-#define PHP_PROPRO_VERSION "1.0.0"
+#define PHP_PROPRO_VERSION "0.1.0"
 
 #ifdef PHP_WIN32
 #	define PHP_PROPRO_API __declspec(dllexport)
